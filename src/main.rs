080217c36eb04cceec6bdba3bbff6bac540b2649
Nile Jocson <seiversiana@gmail.com>
2026-04-09 16:31:39 +0800

    Add init function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,62 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <seiversiana@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use std::fs;
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use colored::Colorize;
+use directories::ProjectDirs;
+
+
+
+fn selfish_dirs() -> ProjectDirs
+{
+	return ProjectDirs::from("com", "seiversiana", "selfish").expect("Could not retrieve directories");
+}
+
+fn data_dir() -> PathBuf
+{
+	return selfish_dirs().data_local_dir().to_path_buf();
+}
+
+fn init()
+{
+	if data_dir().exists()
+	{
+		println!("{}", "selfish is already initialized!".red());
+		return;
+	}
+
+	fs::create_dir_all(data_dir()).expect("Could not create store");
+
+	println!("{}", "Successfully initialized selfish.".green());
+}
+
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Selfish
+{
+	#[command(subcommand)]
+	command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands
+{
+	Init
+}
+
 
 
 fn main()
 {
-	println!("Hello, world!");
+	let selfish = Selfish::parse();
+
+	match &selfish.command
+	{
+		Commands::Init => init()
+	}
 }
