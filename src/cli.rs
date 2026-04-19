26a6b4340de32da94cdf9faf55197de2b2013289
Nile Jocson <seiversiana@gmail.com>
2026-04-19 18:39:30 +0800

    Add cli structure

---
+++ src/cli.rs
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <seiversiana@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+use clap::{Parser, Subcommand};
+
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Selfish
+{
+	#[command(subcommand)]
+	command: Command
+}
+
+#[derive(Subcommand)]
+pub enum Command
+{
+	Init,
+	Config,
+	Todo,
+	Schedule,
+	Budget
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <seiversiana@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+mod cli;
+
+use clap::Parser;
+
+use crate::cli::Selfish;
+
 
 
 fn main()
 {
-	println!("Hello, world!");
+	run();
+}
+
+fn run()
+{
+	let selfish = Selfish::parse();
 }
